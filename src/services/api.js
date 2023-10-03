09cd3247a8f529ad1ea71ddb8155ae31f8d079d6
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2023-10-02 22:06:32 -0300

    consumo da api e estruturação da home

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,9 @@
-//base url:https://api.themoviedb.org/3
+import axios from "axios";
+//base url:https://api.themoviedb.org/3/
 //url api: /movie/now_playing?api_key=b846534ca04f9d422f16046ceb1a619c&language=pt-BR
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+
+});
+
+export default api;

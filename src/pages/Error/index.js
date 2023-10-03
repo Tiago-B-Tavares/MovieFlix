febedbf44012e8db1193979d123e4295f25134a5
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2023-10-03 14:18:13 -0300

    feat: pagina de not found criada

---
+++ src/pages/Error/index.js
@@ -0,0 +1,13 @@
+import './Error.css';
+import { Link } from 'react-router-dom';
+
+function Error() {
+    return(
+        <div className="not-found">
+            <h1>404</h1>
+            <h2>PÁGINA NÃO EMCONTRADA</h2>
+            <Link to={"/"}>Acesse a lista de filmes</Link>
+        </div>
+    )
+}
+export default Error;
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Header from './components/Header';
-
+import Error from './pages/Error'
 function RoutesApp() {
   return (
     <BrowserRouter>
@@ -10,6 +10,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+        <Route path="*" element={<Error/>} />
       </Routes>
     </BrowserRouter>
   );

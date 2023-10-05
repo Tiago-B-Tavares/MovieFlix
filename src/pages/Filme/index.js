c49d20a98fc2e529f75983d3d3ae507d698a4736
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2023-10-04 22:03:09 -0300

    feat: paginas de detales do filme adicionada

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,8 +1,18 @@
+import { useEfect, useState } from "react";
+import { useParams } from 'react-router-dom';
+
+import api from "../../services/api";
 
 function Filme(){
+  const { id } = useParams();
+  useEfect(()=>{
+    async function loadFilmes(){
+      await api.get("movie/now_playing");
+    }
+  })
   return(
     <div>
-      <h1>BEM VINDO A PAGINA DETALHES DO FILME</h1>
+      <h1>ACESSANDO FILME {id}</h1>
     </div>
   )
 }

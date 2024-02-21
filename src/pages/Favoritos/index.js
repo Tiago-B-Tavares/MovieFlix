b98a70d9ec5e03e06261c48a684be6a9a0ca4b2f
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2024-02-21 13:32:41 -0300

    style: responsividade no mobile
    Foi corrigido o problema na resposividade para o modo mobile

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -31,7 +31,7 @@ function Favoritos(){
             <li key={item.id}>
               <span>{item.title}</span>
 
-              <div>
+              <div className='navegacao'>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
                 <button onClick={()=>excluirFilme(item.id)}>Excluir</button>
               </div>

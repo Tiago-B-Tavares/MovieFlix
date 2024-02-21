9ebb1ef103e5b4fe6be8751917faf5062696c3e5
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2024-02-21 13:56:03 -0300

    style: responsividade no mobile
    Foi corrigido o problema na resposividade para o modo mobile

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,46 +1,49 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'
-import { toast } from 'react-toastify';
-import './favoritos.css';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import "./favoritos.css";
 
-function Favoritos(){
-  const [filmes, setFilmes] = useState([])
+function Favoritos() {
+  const [filmes, setFilmes] = useState([]);
 
-  useEffect(()=>{
-
+  useEffect(() => {
     const minhaLista = localStorage.getItem("movie@flix");
-    setFilmes(JSON.parse(minhaLista) || [])
-
-  }, [])
-  function excluirFilme(id){
-    let filtroFilmes = filmes.filter( (item ) => {
-      return (item.id !== id);
-    })
+    setFilmes(JSON.parse(minhaLista) || []);
+  }, []);
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
     setFilmes(filtroFilmes);
     localStorage.setItem("movie@flix", JSON.stringify(filtroFilmes));
-    toast.success("Filme removido com sucesso!")
+    toast.success("Filme removido com sucesso!");
   }
 
-  return(
+  return (
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
-{filmes.length === 0 && <span>Você ainda não favoritou nenhum filme :(</span>}
+      {filmes.length === 0 && (
+        <div className="sem-favoritos">
+          <span>Você ainda não favoritou nenhum filme :(</span>
+          <Link to={"/"}>Voltar ao início</Link>
+        </div>
+      )}
       <ul>
         {filmes.map((item) => {
-          return(
+          return (
             <li key={item.id}>
               <span>{item.title}</span>
 
-              <div className='navegacao'>
+              <div className="navegacao">
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button onClick={()=>excluirFilme(item.id)}>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
-          )
+          );
         })}
       </ul>
     </div>
-  )
+  );
 }
 
 export default Favoritos;
